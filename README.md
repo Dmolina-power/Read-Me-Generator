9354a5c371f52283d0926a014f9f1767b0c4b3be
daniel molina <bigdpower@icloud.com>
2020-05-29 09:03:13 -0700

    added README

---
+++ README.md
@@ -0,0 +1,43 @@
+  ![GitHub followers](https://img.shields.io/github/followers/dmolina-power) 
+  ![GitHub repo size](https://img.shields.io/github/repo-size/dmolina-power/Read-Me-Generator) 
+  
+  # Read-ME-Generator
+
+## Table of Contents:
+ * [Description](#description)
+ * [Installation](#installation)
+ * [Usage](#usage)
+ * [Project Link](#projectLink)
+ * [Contact](#email) 
+ * [Contributing](#contributing)
+ * [License](#license)
+ 
+ ## Description
+ Here's a simple README generator that helps build a simple read me. Gives details on description, Installation, Usage, Contributing, and Licenses.
+ and puts together a simple layout to get started!
+
+ ## Screen Shots
+
+
+ ## Installation
+ none
+
+ ## Usage
+ Stuck on a blank README page use this README generator to help you get started! just simply answer questions given to you and it will layout the format for you.
+
+ ## ProjectLink
+ https://dmolina-power.github.io/Read-Me-Generator/
+
+ ## Email
+ bigdpower@icloud.com
+
+ 
+
+ ## Contributing
+ none
+
+ ## License
+ none
+
+
+ ![Avatar Image](https://avatars0.githubusercontent.com/u/62960620?v=4)
--- index.js
+++ index.js
@@ -1,96 +1,98 @@
 const fs = require("fs");
 const util = require("util");
-const axios = require("axios")
+const axios = require("axios");
 const inquirer = require("inquirer");
 const markdown = require("./utils/generateMarkdown");
 
 const writeFile = util.promisify(fs.writeFile);
 
-
-
 // generate Questions //
 const questions = [
-
-
-{
+  {
     name: "username",
     type: "input",
     message: "What is your Github username?",
-},
-{
+  },
+  {
     name: "email",
     type: "input",
     message: "What is your Email address?",
-},
+  },
 
-{
+  {
     name: "title",
-      type: "input",
-      message: "Name of the project Title",
-    },
-    {
-        name: "projectLink",
-        type: "input",
-        message: "whats your project link URL?",
-      },
-    {
-      name: "description",
-      type: "input",
-      message: "describe your project:",
-    },
-    {
-      name: "installation",
-      type: "input",
-      message: "steps in installing your project are?",
-    },
-    {
-      name: "usage",
-      type: "input",
-      message: "how to use the repo:",
-    },
-    {
-      name: "license",
-      type: "list",
-      message: "which license do you use?",
-      choices: ["MIT", "IBM"],
-    },
-    {
-      name: "contributing",
-      type: "input",
-      message: "mention any collaborators in your project",
-    },
-]
-    function init() {
-        questionAsk();
-    }
-    
-    init();
-  
-    function writeToFile(fileName, data) {
-        fs.writeFile(fileName, data, function(err) {
-            
-            if (err) {
-                return console.log(err);
-            }
-            console.log("success!");
-          });
-    }
-        
-   
-        function questionAsk(){
+    type: "input",
+    message: "Name of the project Title",
+  },
+  {
+    name: "projectLink",
+    type: "input",
+    message: "whats your project link URL?",
+  },
+  {
+    name: "description",
+    type: "input",
+    message: "describe your project:",
+  },
+  {
+    name: "installation",
+    type: "input",
+    message: "steps in installing your project are?",
+  },
+  {
+    name: "usage",
+    type: "input",
+    message: "how to use the repo:",
+  },
+  {
+    name: "license",
+    type: "list",
+    message: "which license do you use?",
+    choices: ["MIT", "IBM"],
+  },
+  {
+    name: "contributing",
+    type: "input",
+    message: "mention any collaborators in your project",
+  },
+];
+function init() {
+  questionAsk();
+}
 
-            inquirer.prompt(questions).then((answers) => {
-                const {username, title, description, installation, usage, projectLink, email, contributing , license} = answers;
-                let queryURL = `https://api.github.com/users/${username}?per_page=100`
-                axios.get(queryURL).then(function(response){
-                    let imageURL = response.data.avatar_url;
-                    
-   
-            let markdown =` 
+init();
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("success!");
+  });
+}
+
+function questionAsk() {
+  inquirer.prompt(questions).then((answers) => {
+    const {
+      username,
+      title,
+      description,
+      installation,
+      usage,
+      projectLink,
+      email,
+      contributing,
+      license,
+    } = answers;
+    let queryURL = `https://api.github.com/users/${username}?per_page=100`;
+    axios.get(queryURL).then(function (response) {
+      let imageURL = response.data.avatar_url;
+
+      let markdown = ` 
   
   ![GitHub followers](https://img.shields.io/github/followers/${username}) 
   ![GitHub repo size](https://img.shields.io/github/repo-size/${username}/${title}) 
-  ![GitHub repo size](https://img.shields.io/github/repo-size/${username}/${title})
+  
   
   
 # ${title}
@@ -130,7 +132,7 @@ const questions = [
 
  ![Avatar Image](${imageURL})
  `;
- writeToFile("./GenerateREADME.md", markdown);
-});
-})
+      writeToFile("./GenerateREADME.md", markdown);
+    });
+  });
 }
